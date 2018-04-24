fc5f528c14aacf423e680472a7bdbd56612d2c06
hansgaensbauer <htgaens@gmail.com>
2018-04-23 19:56:35 -0600

    Swapped versions of kandao

--- Compare.py
+++
@@ -1,6 +0,0 @@
-from skimage.measure import structural_similarity as ssim
-
-if ssim(imageA, imageb) >= 0.5:
-    print(match)
-else:
-    print(no match)
--- Test Compare.py
+++
@@ -1,1 +0,0 @@
-# compare imageA and imageB
